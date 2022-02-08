b68ce8300fce3c263a25143ce37f6bc88c34812f
Emil Gardström <emil.gardstrom@gmail.com>
2022-02-08 18:14:07 +0100

    skip rustfmt on directives for tracing

--- src/util.rs
+++ src/util.rs
@@ -46,6 +46,7 @@ fn install_tracing() {
     use tracing_subscriber::{fmt, EnvFilter};
 
     let fmt_layer = fmt::layer().with_target(true);
+    #[rustfmt::skip]
     let filter_layer = EnvFilter::try_from_default_env()
         .or_else(|_| EnvFilter::try_new("info"))
         .map(|f| {
